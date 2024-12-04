10eeb3979e18e0a919e78c8792bd967bd72f3ba6
suryansh <suryanshnema25@gmail.com>
2024-12-04 23:47:16 +0530

    requirements.txt

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'poseDetection',
+    version= '0.0.0',
+    author= 'suryansh nema',
+    author_email= 'suryanshnema03@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
